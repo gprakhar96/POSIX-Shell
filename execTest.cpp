a48bef4bf42e401c799ad740e78031f1838d8459
aadesh <adesh.ingle74@gmail.com>
2022-11-25 00:19:50 +0530

    run command from bin folder working

--- execTest.cpp
+++ execTest.cpp
@@ -8,40 +8,15 @@ void runCommand(vector<string> inpCommand) {
   char *arr[inpCommand.size() + 1];
   for (int i = 0; i < inpCommand.size(); i++) {
     arr[i] = inpCommand[i].data();
-  }
+  }s
   char *n = {NULL};
   arr[inpCommand.size()] = n;
-  // for (int i = 0; i < inpCommand.size(); i++) {
-  //   cout << arr[i] << endl;
-  // }
-  // char *arr[] = {"cat", "/home/aadesh/Downloads/posixShell/execTest.cpp",
-  // NULL};
-
   execvp(arr[0], arr);
 }
 
 int main(int argc, char *argv[], char *envp[]) {
 
-  //  string command;
-  //  cin >> command;
   vector<string> inpCommand;
-  // command = "ping www.google.com";
-  // std::istringstream ss(command);
-  // std::string token;
-
-  // while (std::getline(ss, token, ' ')) {
-  //   cout << token << endl;
-  //   inpCommand.push_back(token);
-  // }
-  // int size = inpCommand.size();
-  // for (int i = 0; i < size; i++) {
-  //   cout << inpCommand[i];
-  // }
-
-  // inpCommand.push_back("printenv");
-  // inpCommand.push_back("/home/aadesh/peer.cpp");
-  // inpCommand.push_back("/home/aadesh/tracker.cpp");
-  // inpCommand.push_back("testDirectory");
   inpCommand.push_back("ping");
   inpCommand.push_back("www.google.com");
   runCommand(inpCommand);
