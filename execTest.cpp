e561db9363569101a7073ebcf368eac3eff97241
aadesh <adesh.ingle74@gmail.com>
2022-11-25 00:27:29 +0530

    last commit bin

--- execTest.cpp
+++ execTest.cpp
@@ -11,14 +11,25 @@ void runCommand(vector<string> inpCommand) {
   }
   char *n = {NULL};
   arr[inpCommand.size()] = n;
+
   execvp(arr[0], arr);
 }
 
 int main(int argc, char *argv[], char *envp[]) {
 
   vector<string> inpCommand;
-  inpCommand.push_back("ping");
-  inpCommand.push_back("www.google.com");
+  // inpCommand.push_back("printenv");
+  // inpCommand.push_back("diff");
+  // inpCommand.push_back("/home/aadesh/peer.cpp");
+  // inpCommand.push_back("/home/aadesh/tracker.cpp");
+  // inpCommand.push_back("mkfir");
+  // inpCommand.push_back("/home/aadesh/testDirectory");
+  // inpCommand.push_back("pwd");
+  // inpCommand.push_back("mkdir");
+  // inpCommand.push_back("/home/aadesh/Downloads/posixShell/demoDirectory11");
+  // inpCommand.push_back("ping");
+  // inpCommand.push_back("www.google.com");
+  inpCommand.push_back("ifconfig");
   runCommand(inpCommand);
   return 0;
 }
